51abbd7a09368d3193fe0693f78c68d1c17e370a
Penpen7 <Raosys@live.jp>
2022-06-13 00:45:50 +0900

    モーダルを修正

--- src/Edit.js
+++ src/Edit.js
@@ -12,7 +12,6 @@ const Modal = ({show, setShow, addTodo}) => {
   }
 
   const onClickOK = (e) => {
-    console.log(title, detail)
     addTodo(title, detail)
     setShow(false)
   }
@@ -21,17 +20,42 @@ const Modal = ({show, setShow, addTodo}) => {
     setShow(false)
   }
 
+  const onKeyPress = (e) => {
+    console.log(e.keyCode)
+    if (e.keyCode === 27) {
+      setShow(false)
+    }
+  }
+
   return (
     show ?
-      <div id="overlay">
-        <div id="content">
-          <h1>Edit</h1>
-          <input type="text" value={title} onChange={onChangeTitle} />
-          <input type="text" value={detail} onChange={onChangeDetail} />
-          <button onClick={onClickOK}>OK</button>
-          <button onClick={onClickCancel}>Cancel</button>
+      <div id="overlay" onKeyPress={onKeyPress}>
+        <div role="dialog"
+          aria-labelledby="dialog_label"
+          aria-modal
+        >
+          <div id="content">
+            <h1 id="dialog_label">Add</h1>
+            <div className="dialog_form_item">
+              <label>
+                <span className="label_text">Title</span>
+                <input type="text" value={title} onChange={onChangeTitle} className="input_text" />
+              </label>
+            </div>
+            <div className="dialog_form_item">
+              <labal>
+                <span className="label_text">detail</span>
+                <input type="text" value={detail} onChange={onChangeDetail} className="input_text" />
+              </labal>
+            </div>
+            <div className="dialog_form_actions">
+              <button onClick={onClickOK}>OK</button>
+              <button onClick={onClickCancel}>Cancel</button>
+            </div>
+          </div>
         </div>
-      </div > : null
+      </div>
+      : null
   )
 }
 
